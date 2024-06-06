11f7c515129b673a95369f6f7f7b390b908a5f94
Dariadariaaa99 <enkhdari88@gmail.com>
2024-06-06 17:57:52 +0800

    fixed

--- datatypes.js
+++ datatypes.js
@@ -1,4 +1,9 @@
-let x = 3; 
-typeof 0 ;
+let x = 3;
+typeof 0;
 typeof "444";
 typeof let car = " ";
+
+console.log(typeof var1)
+
+
+
--- variables.js
+++ variables.js
@@ -17,76 +17,188 @@ let ver1 = undefined;
 // null 
 let x2 = null;
 
-const lines = "First name" 
-"Last name" ,
-"Marital status" , 
-"Country" ,
-"Age"; 
-console.log (lines)
-const 
-// 11-20 dugaar bodlogo 
+const lines = "First name"
+"Last name",
+    "Marital status",
+    "Country",
+    "Age";
+console.log(lines)
+const
+    // 11-20 dugaar bodlogo 
 
-let a = 4
-let b = 3
-let x = 10
-let y = 12
-console.log (a>b && x<y);
-console.log (a>b && x>y);
-console.log (a>b || x<y);
-console.log (a>b || x>y);
-console.log (a<b || x>y);
-let five = ! (4>3);
-console.log (five)
-let six = ! (4>3);
-console.log (six)
-let seven = ! (4>3 && 10<12);
-console.log (seven)
-let eight = ! (4>3 && 10>12);
-console.log (eight)
-let nine = ! (4 === "4")
-console.log (nine)
+    //     let a = 4
+    // let b = 3
+    // let x = 10
+    // let y = 12
+    // console.log(a > b && x < y);
+    // console.log(a > b && x > y);
+    // console.log(a > b || x < y);
+    // console.log(a > b || x > y);
+    // console.log(a < b || x > y);
+    // let five = !(4 > 3);
+    // console.log(five)
+    // let six = !(4 > 3);
+    // console.log(six)
+    // let seven = !(4 > 3 && 10 < 12);
+    // console.log(seven)
+    // let eight = !(4 > 3 && 10 > 12);
+    // console.log(eight)
+    // let nine = !(4 === "4")
+    // console.log(nine)
 
-// 1. 936-г 12д хуваагаад гарсан хариуг 9д хуваахад гарах үлдэгдлийг хэвлэнэ үү.
+    // 1. 936-г 12д хуваагаад гарсан хариуг 9д хуваахад гарах үлдэгдлийг хэвлэнэ үү.
 
-let var1 = 936
-let var2 = 12 
-let var3 = (var1%var2)
-console.log (`residual: ${var3}`)
+    let var1 = 936
+let var2 = 12
+let var3 = (var1 % var2)
+console.log(`residual: ${var3}`)
 
 // 2. Гараас 0-100 хооронд тоон утга авч тохирох үсгэн тэмдэглэгээгээр онц эсвэл сайн эсэхийг буцаа. (A эсвэл B)
+if (year % 4 == 0 && year % 100 != 0)
 
 
 
-
-// 3. Он, сараа гараас оруулахад тухайн сар хэд хоногтой болохыг харуул.
-let month = prompt ('enter the month');
-let year = prompt ('enter the year');
+    // 3. Он, сараа гараас оруулахад тухайн сар хэд хоногтой болохыг харуул.
+    let month = prompt('enter the month');
+let year = prompt('enter the year');
 
 if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
     console.log(`${year} is a leap year`)
 } else {
-    console.log (`${year} + "is common year"`)
+    console.log(`${year} + "is common year"`)
 }
 January
 February
 March
 April
-May 
-June 
+May
+June
 July
-August 
-September 
-October 
-November 
-December 
+August
+September
+October
+November
+December
 
 
 let days = (`${month} has 31 days in ${year}.`);
-console.log (num)
+console.log(num)
 
-4. Өгөгдсөн эерэг тоо нь 3 эсвэл 7-ын үржвэр эсэхийг шалгана уу.
+4. Өгөгдсөн эерэг тоо нь 3 эсвэл 7 - ын үржвэр эсэхийг шалгана уу.
 5. Өгөгдсөн тооны тэгш эсвэл сондгой эсэхийг шалгана уу.
 6. Өгөгдсөн тооны эерэг тоо эсвэл сөрөг тоо болохыг шалгаад хэвлэж харуулна уу.
-7. 4 н хувьсагч зарлан тоон утга өг. Өгөгдсөн 4 тооны 80-аас их тоонуудын нийлбэрийг ол.
+7. 4 н хувьсагч зарлан тоон утга өг.Өгөгдсөн 4 тооны 80 - аас их тоонуудын нийлбэрийг ол.
 
 
+// a>b unen bol A g hevleh 
+
+// let aa= 3
+// let bb= 2
+// if (aa>bb)
+//     {console.log (`A=3`) 
+//     }
+//     else {
+//         console.log ("B=2")
+//     }
+
+// // const aa = promt ("aa-iin utgiig oruulna uu")
+// // const bb = promt ("bb-iin utgiig oruulna uu")
+// // if (aa>bb)
+// //     {console.log (`A=3`) 
+// //         }
+// //         else {
+// //             console.log ("B=2")
+//         }
+
+// 2 toonii ihiig hevleh 
+
+// let aa = 2
+// let bb = 8
+// if (aa > bb) {
+//     console.log(` ih too = ${aa}`)
+// }
+// else {
+//     console.log(` ih too = ${bb}`)
+// }
+
+// 3 toonii hamgiin ih 
+
+let aaa = 3
+let bbb = 9
+let ccc = 10
+if (aaa > bbb && aaa > ccc) {
+    console.log("aaa")
+}
+else if (bbb > ccc && bbb > aaa) {
+    console.log("bbb")
+}
+else {
+    console.log("ccc")
+}
+
+
+// 4 toonii 3 dah ih toog oloh 
+
+let ab1 = 1
+let ab2 = 3
+let ab3 = 10
+let ab4 = 9
+if (ab1 > ab2 && ab1 > ab3 && ab1 < ab4) {
+    console.log("ab1")
+}
+else if (ab2 > ab1 && ab2 > ab3 && ab2 < ab4) {
+    console.log("ab2")
+}
+else if (ab3 > ab1 && ab3 > ab2 && ab3 < ab4) {
+    console.log("ab3")
+}
+else { console.log("ab4") }
+
+// 2 toonii ih no if statement 
+let p = 9
+let o = 10
+console.log(math.abs == (p - o))
+
+
+
+// triangle 
+
+let q = 10
+let w = 90
+let e = 3
+if (q + w > e && q - w < e) { console.log(`triangle`) }
+else if (q + e > w && q - e < w) { console.log(`triangle`) }
+else if (w + e > q && w - e < q) { console.log(`triangle`) }
+else { console.log(`not triangle`) }
+
+
+// even odd 
+let k = 90
+if (k % 2 == 0) {
+    console.log(`even number`)
+}
+else (console.log(`odd number`))
+
+// 3 toog erembel 
+let x11 = 1
+let x12 = 2
+let x13 = 3
+
+if (x11 > x12 && x11 > x13)
+    {console.log (if (x12 > x13) { console.log("x11>x12>13") }
+else (console.log("x11>x13>x12")))}
+    if (x12 > x13) { console.log("x11>x12>13") }
+    else (console.log("x11>x13>x12"))
+else if (x12 > x11 && x12 > x13)
+    if (x11 > x13) { console.log("x12>x11>13") }
+    else (console.log("x11>x13>x11"))
+else (x12 > x11 && x12 > x13)
+    (if (x11 > x13) { console.log("x12>x11>13") }
+    else (console.log("x11>x13>x11")))
+
+// tegsh untsugt triangle areas 
+
+let h = 10
+let j = 12
+let g = (h ** 2 + j ** 2) ** 0.5
+console.log(0.5 * g * j)
